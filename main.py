eec27395f3e105518bb4a595c266bbae0f70a67a
Mihranw <mihranw@gmail.com>
2023-06-01 19:12:08 +0330

    fix project and files

--- main.py
+++ main.py
@@ -6,64 +6,40 @@ from email.mime.application import MIMEApplication
 from colorama import Fore
 from time import sleep
 
-your_email = "abs.tbzmed@gmail.com"
-your_password = "spikbkkpatwjzndl"
+your_email = "yourmail@mail.com" # Your Email
+your_password = "password" # Your Email Password => App Password with Gmail SMTP
 
 server = smtplib.SMTP('smtp.gmail.com', 587, timeout=3600)
 server.starttls()
 server.ehlo()
 server.login(your_email, your_password)
 
-email_list = pd.read_excel('emails.xlsx')
+email_list = pd.read_excel('Emails.xlsx')
 
 emails = email_list['Email']
-message = """فعال محترم عرصه نانو
+# Your Email Message
+message = """Hi
+This message is for testing
 
-
-سلام علیکم
-     همانگونه که مستحضرید در حوزه نوآوری و کارآفرینی، توجه به توانایی ­ها و مهارت ­های فردی امری بسیار مهم می­ باشد.
-فلذا پرسشنامه­ ی پیش رو به منظور بررسی توانمندی های افراد جهت استفاده از فرصت هایی که در زمینه کارآفرینی و فناوری نانو برای آن ها پیش می آید ایجاد گردیده است.
-پرسشنامه مذکور داری 33 سوال می باشد و در انتهای نامه پیوست گردیده، لذا با تخصيص زمان ارزشمندتان به طور دقيق آنرا تکميل نمایید.
-شايان ذکر است اين اطلاعات کاملاً محرمانه تلقي شده و صرفاً جهت دستيابي به اهداف پژوهش به صورت کلي مورد بررسی قرار خواهد گرفت.
-در ضمن با توجه به اینکه این پرسشنامه جهت بررسی وضعیت موجود است و بر اساس آن در رابطه با سیاست گذاری و نقشه راه مربوطه تصمیم گیری خواهد شد؛ خواهشمند است با پرهیز از ایده آل گرایی و به طور شفاف وضعیت موجود هریک از سوالات را در پاسخ اظهار نمایید.
-
-
-پيشاپيش از همکاري صميمانه شما کمال تشکر و قدردانی را داریم. 
-
-
-
-
-لینک پرسشنامه:
-https://survey.porsline.ir/s/qyJq56Q
-
-
-
-زهرا اکبری مقدم 
-
-دانشجو ارشد نانو فناوری پزشکی 
-
-دانشکده علوم نوین پزشکی 
-
-دانشگاه علوم پزشکی تبریز
-
-z.akbarimoghadam77@gmail.com"""
-fs = open('success.txt', 'w')
-fe = open('errors.txt', 'w')
+mihranw@gmail.com
+"""
+fs = open('success.txt', 'w') # For Log
+fe = open('errors.txt', 'w') # For Log
 
 for i in range(len(emails)):
     try:
         msg = MIMEMultipart()
-        msg['Subject'] = 'بررسي توانایی و مهارت افراد در حوزه کارآفرینی(برای فعالین عرصه نانو)'
+        msg['Subject'] = 'Your Email Subject'
         msg['From'] = your_email
         msg['To'] = emails[i]
         msg.attach(MIMEText(message))
         server.sendmail(your_email, emails[i], msg.as_string())
-        fs.write(f'{i + 1} : {emails[i]}\n')
+        fs.write(f'{i + 1} : {emails[i]}\n') # Add To Log File
         print(Fore.GREEN + f"{i + 1}) Email Was Successfully Sent To : ", emails[i])
         sleep(2)
     except Exception as e:
         fe.write(f'{i + 1} : {emails[i]}\n')
-        fe.write(f'{e}\n')
+        fe.write(f'{e}\n') # Add To Log File
         fe.write(f'---------------------------------\n')
         print(Fore.RED + f"{i + 1}) There Was An Error Sending Email To : ", emails[i])
 
